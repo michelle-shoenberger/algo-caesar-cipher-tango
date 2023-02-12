10666eea69a34283c342bad3d2f38f95728f2f1a
mickey <shoey25ms@gmail.com>
2023-02-11 21:27:50 -0500

    My answer

--- js/caesarCipher.js
+++ js/caesarCipher.js
@@ -1,3 +1,22 @@
-exports.caesarCipher = function() {
-
+exports.caesarCipher = function(s, amount) {
+  let ans = [];
+  for (let i=0; i<s.length; i++) {
+    let old_ord = s.toLowerCase().charCodeAt(i);
+    if (old_ord >= 97 && old_ord <= 122) {
+      let shifted = old_ord + amount;
+      if (shifted < 97) {
+        shifted += 26;
+      } else if (shifted > 122) {
+        shifted -= 26;
+      }
+      if (s[i] === s[i].toUpperCase()) {
+        ans.push(String.fromCharCode(shifted).toUpperCase())
+      } else {
+        ans.push(String.fromCharCode(shifted))
+      }
+    } else {
+      ans.push(s[i])
+    }
+  }
+  return ans.join("")
 };
--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,15 @@
 def caesar_cipher(string, shift_amount):
+    ans =[]
+    for char in string:
+        if char.isalpha():
+            new = ord(char.lower()) + shift_amount
+            if new < 97: new += 26
+            if new > 122: new -= 26
+            if char == char.upper():
+                ans.append(chr(new).upper())
+            else:
+                ans.append(chr(new))
+        else:
+            ans.append(char)
+
+    return "".join(ans)
